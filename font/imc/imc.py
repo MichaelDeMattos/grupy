4d69858569d421e751495c33e07921ce5f1a6c4a
Michael <chelmto3000@gmail.com>
2020-09-09 22:36:51 -0300

    Finalizaçao do crud

--- font/imc/imc.py
+++ font/imc/imc.py
@@ -7,17 +7,20 @@ import sys
 import sqlite3
 import traceback
 import zlib
+import datetime
 import base64
+import time
 
 gi.require_version('Gtk', '3.0')
-DEBUG = False
+DEBUG = True
 if DEBUG == True:
-	os.system("rm -rf ~/grupy/font/imc/UtilGtk; ln -s ~/grupy/util/compiled/ UtilGtk")
+	os.system("rm -rf ~/grupy/font/imc/UtilGtk; ln -s ~/grupy/util/compiled/ ~/grupy/font/imc/UtilGtk")
 
 from gi.repository import Gtk, Gdk
 from UtilGtk.mikeUtil import MikeGtk
 
 builder = Gtk.Builder()
+init = 0
 
 if DEBUG == True:
 	builder.add_from_file("imc.ui")
@@ -66,6 +69,9 @@ class Handler(MikeGtk):
 		self.conexao = sqlite3.connect(self.path_database)
 		self.cursor = self.conexao.cursor()
 		
+		# -- Variável global utilizada para excluir registro do IMC
+		self.cod_imc_select = []
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 	# -- Ao fechar a janela principal
 	def on_main_window_destroy(self, *args):
@@ -135,21 +141,102 @@ class Handler(MikeGtk):
 	# -- Faz  a busca no banco de dados e mostra na grade o resultado
 	def update_treeview(self, *args):
 		self.list_imc.clear()
-		sql_search_imc_history = (" select name, current_weight,"
+		sql_search_imc_history = (" select cod_history_imc, name,"
+								  " current_weight,"
 								  " height, imc, imc_result"
 								  " from history_imc")
 		self.cursor.execute(sql_search_imc_history)
 		search_imc_history = self.cursor.fetchall()
 		for imc_history in search_imc_history:
-			name = imc_history[0]
-			current_weight = str(round(imc_history[1],2))
-			heigth = str(round(imc_history[2],2))
-			imc = str(round(imc_history[3],2))
-			imc_result = imc_history[4]
+			select = False
+			cod_imc = str(imc_history[0])
+			name = imc_history[1]
+			current_weight = str(round(imc_history[2],2))
+			heigth = str(round(imc_history[3],2))
+			imc = str(round(imc_history[4],2))
+			imc_result = imc_history[5]
 			
-			lst_imc_history = (name, current_weight, heigth, imc, imc_result)
+			lst_imc_history = (select, cod_imc, name, current_weight, heigth, imc, imc_result)
 			self.list_imc.append(lst_imc_history)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# -- Ao editar a coluna nome na grade
+	def on_render_name_edited(self, *args):
+		try:
+			cols = {"select": 0, "cod_imc": 1, "name": 2,"current_weight": 3,
+					"heigth": 4, "imc": 5, "imc_result": 6}
+			line = args[1]
+			new_value = args[2]
+			cod_imc_history = self.treeview_imc.get_model()[line][cols["cod_imc"]]
+
+			sql_update_name = (" update history_imc"
+							   " set name = :name"
+							   " where cod_history_imc = :cod_imc")
 			
+			sql_args = {"name": new_value, "cod_imc": cod_imc_history}
+			
+			self.cursor.execute(sql_update_name, sql_args)
+			self.conexao.commit()
+			self.simple_msg_box(self.msg_dialog,title="Sucesso!!",
+							    text=("Registro atualizado com sucesso!"))
+		
+		except Exception as ex:
+			self.simple_msg_box(self.msg_dialog,title="Erro!",
+							    text=("Erro ao atualizar o registro!\n%s" % str(ex)))
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# -- Ao selecionar um item na grade
+	def on_render_select_toggled(self, *args):
+		try:
+			cols = {"select": 0, "cod_imc": 1, "name": 2,"current_weight": 3,
+					"heigth": 4, "imc": 5, "imc_result": 6}
+			
+			line = args[1]
+			value_default = False
+			new_value = True
+			now_value = self.treeview_imc.get_model()[line][cols["select"]]
+			cod_imc = self.treeview_imc.get_model()[line][cols["cod_imc"]]
+			
+			if now_value == False:
+				self.cod_imc_select.append(cod_imc)
+				print(self.cod_imc_select)
+				self.treeview_imc.get_model()[line][cols["select"]] = new_value
+			else:
+				self.treeview_imc.get_model()[line][cols["select"]] = value_default
+				remove_value = self.treeview_imc.get_model()[line][cols["cod_imc"]]
+				self.cod_imc_select.remove(remove_value)
+
+		except Exception as ex:
+			self.simple_msg_box(self.msg_dialog,title="Erro!",
+							    text=("Erro ao selecionar o registro!\n%s" % str(ex)))
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# -- Ao clicar no botão excluir historico IMC
+	def on_bt_delete_history_imc_clicked(self, *args):
+		try:
+			if self.cod_imc_select != [] or self.cod_imc_select == None:
+				for i in self.cod_imc_select:
+					sql_delete_imc_history = (" delete from history_imc"
+									  	      " where cod_history_imc"
+									 	      " in (:cod_imc)")
+					sql_args = {"cod_imc": i}
+
+					self.cursor.execute(sql_delete_imc_history, sql_args)
+					self.conexao.commit()
+					self.simple_msg_box(self.msg_dialog,title="Sucesso!",
+							    		text=("Registro's excluído's com sucesso!\n"))
+					self.cod_imc_select.remove(i)
+					self.update_treeview()
+
+		except Exception as ex:
+			self.simple_msg_box(self.msg_dialog,title="Erro!",
+							    	text=("Erro ao excluir o registro!\n%s" % str(ex)))
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# -- Ao clicar em buscar atualiza a grade
+	def on_bt_search_clicked(self, *args):
+		self.update_treeview()
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 	# -- Ao fechar a janela calcular imc
 	def on_calculate_imc_delete_event(self, *args):
@@ -158,5 +245,5 @@ class Handler(MikeGtk):
 		return True
 		
 if __name__ == '__main__':
-    builder.connect_signals(Handler())
-    Gtk.main()
+	builder.connect_signals(Handler())
+	Gtk.main()
